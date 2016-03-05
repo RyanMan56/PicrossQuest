afcb74b493ab258d37d9c916f1165d49f4bf3d4f
Ryan Jones <RyanMan56@gmail.com>
2016-03-05 00:25:03 +0000

    Current Progress

---
+++ core/src/com/subzero/picrossquest/services/ImageProvider.java
@@ -0,0 +1,14 @@
+package com.subzero.picrossquest.services;
+
+public class ImageProvider {
+	private float SCREEN_WIDTH = 854f;
+	private float SCREEN_HEIGHT = 480f;
+	
+	public float getScreenWidth(){
+		return SCREEN_WIDTH;
+	}
+	public float getScreenHeight(){
+		return SCREEN_HEIGHT;
+	}
+	
+}
